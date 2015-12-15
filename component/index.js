798ca3f0bdcc220a583d1984d51aeb70fb18be98
Rom Chatterjee <rom.chatterjee@smokeball.com>
2015-12-15 15:57:51 +1100

    Fix generator path for component

--- component/index.js
+++ component/index.js
@@ -63,7 +63,7 @@ module.exports = scriptBase.extend({
           } else if (fs.existsSync(path.join(process.cwd(), 'app'))) {
             componentPath = path.join('app', 'components', kebab);
           } else if (fs.existsSync(path.join(process.cwd(), 'www'))) {
-            componentPath = path.join('www', 'services', kebab);
+            componentPath = path.join('www', 'components', kebab);
           } else {
             componentPath = path.join('components', kebab);
           }
